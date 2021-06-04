0fbb5f3857478eaf3585ea055b52e7aa0ce41d8d
Tejas Shastry <tejashastry@gmail.com>
2021-06-04 10:15:20 -0400

    more FE work
    updates

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,11 @@ import './App.css';
 
 class App extends Component {
 
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
   async loadWeb3() {
     if (window.ethereum) {
       window.web3 = new Web3(window.ethereum)
@@ -16,6 +21,13 @@ class App extends Component {
     else {
       window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
     }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    //Load accounts
+    const accounts = await web3.eth.getAccounts()
+    console.log(accounts) 
   }
 
   render() {
